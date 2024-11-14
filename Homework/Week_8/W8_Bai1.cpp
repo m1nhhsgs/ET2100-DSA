73e8aa54bc7f69b19c6ace60c9e18b2e76cf30cf
Minh <minhhsgs2k4@gmail.com>
2024-11-14 21:27:54 +0700

    update bai 1 W8 va bai 2 W9

--- Homework/Week_8/W8_Bai1.cpp
+++ Homework/Week_8/W8_Bai1.cpp
@@ -8,41 +8,168 @@ In ra tổng của 2 đa thức h(x) = f(x) + g(x) (*)
 using namespace std;
 
 struct Node{
-    int data;
+    int coef;
     int power;
     Node* next;
 };
 
 typedef Node* Polynomial;
-Node* createNode(int x, int y){
-    Node* p = new Node;
-    p->data = x;
-    p->power = y;
-    p->next = NULL;
-    return p;
+
+//Cac node ma co coef = 0 hoac cac node co cung power, tong cac coef = 0 thi se khong hien thi, bi xoa khoi bo nho
+
+void inputPolynomial(Polynomial &p){
+    int n;
+    cout << "Nhap so luong node: ";
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int coef, power;
+        cout << "Nhap he so: ";
+        cin >> coef;
+        cout << "Nhap so mu: ";
+        cin >> power;
+        if(coef != 0){
+            Node* temp = new Node;
+            temp->coef = coef;
+            temp->power = power;
+            temp->next = NULL;
+            if(p == NULL){
+                p = temp;
+            }
+            else{
+                Node* last = p;
+                while(last->next != NULL){
+                    last = last->next;
+                }
+                last->next = temp;
+            }
+        }
+    }
 }
 
-void insertTail(Node* &head, int x, int y){
-    Node* p = createNode(x, y);
-    if (head == NULL){
-        head = p;
-        return;
+//Ham in da thuc, neu he so am thi in ra dau tru, neu he so la 1 thi khong in ra he so, neu he so la 0 thi khong in ra node do
+void printPolynomial(Polynomial p){
+    Node* temp = p;
+    while(temp != NULL){
+        cout << temp->coef << "x^" << temp->power;
+        if(temp->next != NULL){
+            cout << " + ";
+        }
+        temp = temp->next;
     }
-    Node* q = head;
-    while (q->next != NULL){
-        q = q->next;
-    }
-    q->next = p;
+    cout << endl;
 }
 
-void printPolynomial(Polynomial head){
-    Polynomial p = head;
-    while (p != NULL){
-        if (p->data )
+//Ham tinh tong cua 2 da thuc, cong cac he so cung so mu
+Polynomial sumPolynomial(Polynomial f, Polynomial g){
+    Polynomial h = NULL;
+    Node* temp1 = f;
+    Node* temp2 = g;
+    while(temp1 != NULL && temp2 != NULL){
+        if(temp1->power == temp2->power){
+            Node* temp = new Node;
+            temp->coef = temp1->coef + temp2->coef;
+            temp->power = temp1->power;
+            temp->next = NULL;
+            if(h == NULL){
+                h = temp;
+            }
+            else{
+                Node* last = h;
+                while(last->next != NULL){
+                    last = last->next;
+                }
+                last->next = temp;
+            }
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        else if(temp1->power > temp2->power){
+            Node* temp = new Node;
+            temp->coef = temp1->coef;
+            temp->power = temp1->power;
+            temp->next = NULL;
+            if(h == NULL){
+                h = temp;
+            }
+            else{
+                Node* last = h;
+                while(last->next != NULL){
+                    last = last->next;
+                }
+                last->next = temp;
+            }
+            temp1 = temp1->next;
+        }
+        else{
+            Node* temp = new Node;
+            temp->coef = temp2->coef;
+            temp->power = temp2->power;
+            temp->next = NULL;
+            if(h == NULL){
+                h = temp;
+            }
+            else{
+                Node* last = h;
+                while(last->next != NULL){
+                    last = last->next;
+                }
+                last->next = temp;
+            }
+            temp2 = temp2->next;
+        }
     }
+    while(temp1 != NULL){
+        Node* temp = new Node;
+        temp->coef = temp1->coef;
+        temp->power = temp1->power;
+        temp->next = NULL;
+        if(h == NULL){
+            h = temp;
+        }
+        else{
+            Node* last = h;
+            while(last->next != NULL){
+                last = last->next;
+            }
+            last->next = temp;
+        }
+        temp1 = temp1->next;
+    }
+    while(temp2 != NULL){
+        Node* temp = new Node;
+        temp->coef = temp2->coef;
+        temp->power = temp2->power;
+        temp->next = NULL;
+        if(h == NULL){
+            h = temp;
+        }
+        else{
+            Node* last = h;
+            while(last->next != NULL){
+                last = last->next;
+            }
+            last->next = temp;
+        }
+        temp2 = temp2->next;
+    }
+    return h;
 }
 
 
 int main(){
-
+    Polynomial f = NULL;
+    Polynomial g = NULL;
+    Polynomial h = NULL;
+    cout << "Nhap da thuc f(x): " << endl;
+    inputPolynomial(f);
+    cout << "Nhap da thuc g(x): " << endl;
+    inputPolynomial(g);
+    cout << "Da thuc f(x): ";
+    printPolynomial(f);
+    cout << "Da thuc g(x): ";
+    printPolynomial(g);
+    h = sumPolynomial(f, g);
+    cout << "Da thuc h(x) = f(x) + g(x): ";
+    printPolynomial(h);
+    return 0;
 }
